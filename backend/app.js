9927d87ad68ef4bac13fca65492c75e9ee23eb72
gautam <buddhgautam777@gmail.com>
2023-03-27 21:51:47 +0530

    final commit

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const app = express();
+const post = require("./routes/post");
+const user = require("./routes/user");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 if (process.env.NODE_ENV !== "production") {
@@ -11,13 +13,12 @@ app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
 
-// Importing Routes
-const post = require("./routes/post");
-const user = require("./routes/user");
-
-// Using Routes
 app.use("/api/v1", post);
 app.use("/api/v1", user);
+
+
+// Importing Routes
+
 
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
